3bf6e4e1c8f023a3c84d8675e5a38e150f591c68
Alfonso BT <ponchobt.dev@gmail.com>
2023-09-21 11:21:29 -0600

    Cool

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,14 +2,12 @@ import { useRoutes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Page404 from "../pages/Page404";
-import Films from "../pages/Films";
 import Header from "../layout/Header";
 
 export default function Router() {
   let element = useRoutes([
-    { path: "home", element: <div><Header/><Home /></div> },
+    { path: "home", element: <div><Header/><Home/></div> },
     { path: "*", element: <Page404 /> },
-    { path: "films", element: <Films /> },
     { path: "/", element: <Login /> },
   ]);
   return element;
